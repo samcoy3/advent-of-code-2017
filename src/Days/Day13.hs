1f2dd2b791d0ed75c1b484721f32911222e7f981
Sam Coy <samcoy3@gmail.com>
2020-11-21 13:52:46 +0000

    Day 13 solutions (uncommented)

--- src/Days/Day13.hs
+++ src/Days/Day13.hs
@@ -10,6 +10,8 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 
+import Data.Function (on)
+
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
@@ -20,19 +22,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = convertToVector <$> scanner `sepBy` endOfLine
+  where
+    scanner = do
+      depth <- decimal
+      asciiCI ": "
+      range <- decimal
+      return (depth, range)
+    convertToVector l = Vec.replicate ((maximum $ fmap fst l) + 1) 0 Vec.// l
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Vector Int
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
-partA = undefined
+partA = foldr1 (+) . Vec.imap severity
+  where
+    severity i val =
+      if i `mod` (2 * (val - 1)) == 0
+        then val * i
+        else 0
 
 ------------ PART B ------------
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB scanners =
+  head
+    . foldr1
+      (.)
+      ( (fmap (\f -> filter f)) . Vec.toList $
+          Vec.imap
+            ( \i v d ->
+                if (v == 0)
+                  then True
+                  else ((i + d) `mod` (2 * (v - 1)) /= 0)
+            )
+            scanners
+      )
+    $ [0 ..]
