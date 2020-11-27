efbfede793b94b776a5bc74039cfdbc3a435e38e
Sam Coy <samcoy3@gmail.com>
2020-11-27 17:55:04 +0000

    Day 14 Part B solutions, uncommented

--- src/Days/Day14.hs
+++ src/Days/Day14.hs
@@ -30,12 +30,49 @@ type Input = String
 
 type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
 partA :: Input -> OutputA
 partA input = length . filter (== '1') . (concatMap (binarifyHash . knotHash . fmap ord . ((input ++ "-") ++) . show)) $ [0 .. 127]
 
 ------------ PART B ------------
+type Coordinate = (Int, Int)
+
+neighbours :: (Coordinate -> Bool) -> Coordinate -> Set Coordinate
+neighbours p (x, y) =
+  Set.fromList
+    . filter p
+    $ [ (x + 1, y),
+        (x, y + 1),
+        (x - 1, y),
+        (x, y - 1)
+      ]
+
+connectedToExisting :: (Coordinate -> Bool) -> Set Coordinate -> Coordinate -> Maybe Coordinate
+connectedToExisting p components coord = connectedToExisting' components (Set.empty) (Set.singleton coord)
+  where
+    connectedToExisting' components prevs currents =
+      let nexts = Set.union currents $ Set.unions (Set.map (neighbours p) currents)
+       in if
+              | currents == nexts -> Just coord
+              | not . Set.null $ Set.intersection nexts components -> Nothing
+              | otherwise -> connectedToExisting' components currents nexts
+
+nestedListsToMap :: [[a]] -> Map (Int, Int) a
+nestedListsToMap = Map.fromList . attachCoords 0 0
+  where
+    attachCoords _ _ ([]) = []
+    attachCoords x _ ([] : ls) = attachCoords (x + 1) 0 ls
+    attachCoords x y ((l : ls) : lss) = ((x, y), l) : (attachCoords x (y + 1) (ls : lss))
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input =
+  let hash = (fmap (binarifyHash . knotHash . fmap ord . ((input ++ "-") ++) . show)) $ [0 .. 127]
+      hashMap = nestedListsToMap hash
+      ones = Map.keys . Map.filter (== '1') $ hashMap
+      findComponents comps [] = comps
+      findComponents comps (x : xs) = case connectedToExisting (\k -> Map.findWithDefault '0' k hashMap == '1') comps x of
+        Nothing -> findComponents comps xs
+        Just y -> findComponents (Set.insert y comps) xs
+   in Set.size $ findComponents Set.empty ones
