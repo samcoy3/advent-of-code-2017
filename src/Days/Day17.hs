24420699d80a5707051d503999924301be0b4986
Sam Coy <samcoy3@gmail.com>
2020-12-01 17:31:47 +0000

    Day 17 solutions (Part A)... again

--- src/Days/Day17.hs
+++ src/Days/Day17.hs
@@ -10,6 +10,9 @@ import qualified Data.Set as Set
 import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 
+import qualified Data.List.PointedList as P
+import qualified Data.List.PointedList.Circular as C
+
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
@@ -20,18 +23,27 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = decimal
 
 ------------ TYPES ------------
-type Input = Void
+type Input = Int
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
 
 ------------ PART A ------------
+insertValues :: Int -> Int -> Int
+insertValues = insertValues' (P.singleton 0) 1
+  where
+    insertValues' plist value target step
+      | value > target = P._focus . C.next $ plist
+      | otherwise =
+        let newPlist = P.insert value . foldr1 (.) (replicate step C.next) $ plist
+         in insertValues' newPlist (value + 1) target step
+
 partA :: Input -> OutputA
-partA = undefined
+partA = insertValues 2017
 
 ------------ PART B ------------
 partB :: Input -> OutputB
