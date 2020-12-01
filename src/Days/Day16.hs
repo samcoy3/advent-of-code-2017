8803e22784c181a347b0a3f565613b60529fde97
Sam Coy <samcoy3@gmail.com>
2020-12-01 08:57:46 +0000

    Day 16, Part A solutions

--- src/Days/Day16.hs
+++ src/Days/Day16.hs
@@ -20,18 +20,50 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = move `sepBy` char ','
+  where
+    move = choice [spin, exchange, partner]
+    spin = char 's' >> decimal >>= return . Spin
+    exchange = do
+      char 'x'
+      a <- decimal
+      char '/'
+      b <- decimal
+      return (Exchange a b)
+    partner = do
+      char 'p'
+      a <- anyChar
+      char '/'
+      b <- anyChar
+      return (Partner a b)
 
 ------------ TYPES ------------
-type Input = Void
+data DanceMove
+  = Spin Int
+  | Exchange Int Int
+  | Partner Char Char
+  deriving (Show)
 
-type OutputA = Void
+type Input = [DanceMove]
+
+type OutputA = String
 
 type OutputB = Void
 
 ------------ PART A ------------
+perform :: DanceMove -> (Vector Char -> Vector Char)
+perform (Spin n) = \v ->
+  let (begin, end) = Vec.splitAt (16 - n) v
+   in end Vec.++ begin
+perform (Exchange a b) = \v ->
+  v Vec.// [(a, v Vec.! b), (b, v Vec.! a)]
+perform (Partner a b) = \v ->
+  let aLoc = fromJust $ Vec.findIndex (== a) v
+      bLoc = fromJust $ Vec.findIndex (== b) v
+   in v Vec.// [(aLoc, b), (bLoc, a)]
+
 partA :: Input -> OutputA
-partA = undefined
+partA moves = Vec.toList . foldr1 (flip (.)) (fmap perform moves) . Vec.fromList $ ['a' .. 'p']
 
 ------------ PART B ------------
 partB :: Input -> OutputB
