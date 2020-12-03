ead82c15fc51e79e9b34d018e9672cd93f002a07
Sam Coy <samcoy3@gmail.com>
2020-12-03 17:22:51 +0000

    Half-hearted effort at Day 20

--- src/Days/Day20.hs
+++ src/Days/Day20.hs
@@ -13,6 +13,9 @@ import qualified Data.Vector as Vec
 import qualified Program.RunDay as R (runDay)
 import Data.Attoparsec.Text
 import Data.Void
+
+import Data.Function (on)
+import Control.Applicative.Combinators as C
 {- ORMOLU_ENABLE -}
 
 runDay :: Bool -> String -> IO ()
@@ -20,18 +23,44 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = undefined
+inputParser = particle `C.sepBy` endOfLine
+  where
+    particle = do
+      string "p="
+      position <- vector3
+      string ", v="
+      velocity <- vector3
+      string ", a="
+      acceleration <- vector3
+      return Particle {..}
+    vector3 = C.between (char '<') (char '>') $ do
+      x <- signed decimal
+      char ','
+      y <- signed decimal
+      char ','
+      z <- signed decimal
+      return Vec3 {..}
 
 ------------ TYPES ------------
-type Input = Void
+data Vec3 = Vec3 {x :: Int, y :: Int, z :: Int} deriving (Eq, Show)
 
-type OutputA = Void
+data Particle = Particle {position :: Vec3, velocity :: Vec3, acceleration :: Vec3} deriving (Eq, Show)
+
+type Input = [Particle]
+
+type OutputA = Int
 
 type OutputB = Void
 
 ------------ PART A ------------
+manhattan :: Vec3 -> Int
+manhattan Vec3 {..} = (abs x) + (abs y) + (abs z)
+
 partA :: Input -> OutputA
-partA = undefined
+partA ps =
+  let ips = zip [0 ..] ps
+      highestAccel = maximumBy (compare `on` (manhattan . acceleration . snd)) ips
+   in fst highestAccel
 
 ------------ PART B ------------
 partB :: Input -> OutputB
